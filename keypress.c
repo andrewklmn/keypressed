176627a9008accabb8b122d5445fb4020c1f1ce7
andrewklmn <user@localhost>
2018-04-02 16:03:53 +0300

    project start

--- keypress.c
+++ keypress.c
@@ -3,39 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char state = 0;
-char state_count = 0;
+#include "keypress.h"
 
-void short_press(){
+char state1 = 0;
+char state_count1 = 0;
+char state2 = 0;
+char state_count2 = 0;
+
+void short_press(unsigned char key){
     //printf(" ===== Short press %d =====\r\n", state_count );
-    RA1 = !RA1;
+    if(key==1) RA2 = 1;
+    if(key==2) RA2 = 0;
 };
 
-void long_press(){
+void long_press(unsigned char key){
     //printf(" ===== long press %d ======\r\n", state_count );
-    RA2 = !RA2;
+    if(key==1) RA3 = 1;
+    if(key==2) RA3 = 0;
 };
 
-void very_long_press(){
+void very_long_press(unsigned char key){
     //printf(" ==== Very long press %d ====\r\n", state_count );
-    RA3 = !RA3;
+    if(key==1) RA4 = 1;
+    if(key==2) RA4 = 0;    
 };
 
 void read_keypress() {
-    if ( RA0 == 0 ) {  
-            if( state == 0 ) {  
-                state = 1; 
-                state_count = 1; 
-            } else { 
-                state_count ++; 
-            }; 
+    if ( BUTTON1 == 0 ) {  
+        if( state1 == 0 ) {  
+            state1 = 1; 
+            state_count1 = 1; 
         } else { 
-            if( state == 1 ){ 
-                if (state_count > 1 && state_count < 10 ) short_press(); 
-                if (state_count > 9 && state_count < 20) long_press(); 
-                if (state_count > 19 ) very_long_press(); 
-                state = 0; 
-                state_count = 0; 
-            };
+            state_count1 ++; 
+        }; 
+    } else { 
+        if( state1 == 1 ){ 
+            if (state_count1 > MIN_SHORT && state_count1 < MAX_SHORT ) short_press(1); 
+            if (state_count1 > MIN_LONG && state_count1 < MAX_LONG) long_press(1); 
+            if (state_count1 > MIN_VERY_LONG ) very_long_press(1); 
+            state1 = 0; 
+            state_count1 = 0; 
         };
+    };
+    if ( BUTTON2 == 0 ) {  
+        if( state2 == 0 ) {  
+            state2 = 1; 
+            state_count2 = 1; 
+        } else { 
+            state_count2 ++; 
+        }; 
+    } else { 
+        if( state2 == 1 ){ 
+            if (state_count2 > MIN_SHORT && state_count2 < MAX_SHORT ) short_press(2); 
+            if (state_count2 > MIN_LONG && state_count2 < MAX_LONG) long_press(2); 
+            if (state_count2 > MIN_VERY_LONG ) very_long_press(2); 
+            state2 = 0; 
+            state_count2 = 0; 
+        };
+    };
 };
--- keypress.h
+++ keypress.h
@@ -8,18 +8,32 @@
 #ifndef KEYPRESS_H
 #define	KEYPRESS_H
 
-char state;
-char state_count;
+#define BUTTON1 RA0
+#define BUTTON2 RA1
 
-void short_press();
-void long_press();
-void very_long_press();
+#define MIN_SHORT 1
+#define MAX_SHORT 10
+#define MIN_LONG 9
+#define MAX_LONG 20
+#define MIN_VERY_LONG 19
+
+
+char state1;
+char state_count1;
+char state2;
+char state_count2;
+
+void short_press(unsigned char);
+void long_press(unsigned char);
+void very_long_press(unsigned char);
 void read_keypress(); 
+
+
 
 
 /* init RA0 button*/
 #define init_keypress()\
-    TRISA = 0b00000001;     
+    TRISA = 0b00000011;
 
 #endif	/* KEYPRESS_H */
 
